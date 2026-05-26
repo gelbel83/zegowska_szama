3dcd41ddcf3a6975c6606026ad8893e0899fcda3
gelbel83 <pewnegorazukinga@gmail.com>
2026-05-26 22:01:42 +0200

    produkty w bazie

--- backup/zegowska_szama.sql
+++ backup/zegowska_szama.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Maj 22, 2026 at 05:20 PM
--- Wersja serwera: 10.4.32-MariaDB
--- Wersja PHP: 8.2.12
+-- Generation Time: May 26, 2026 at 10:01 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,22 +24,78 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `produkt`
+-- Table structure for table `kategoria`
+--
+
+CREATE TABLE `kategoria` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nazwa` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `kategoria`
+--
+
+INSERT INTO `kategoria` (`id`, `nazwa`) VALUES
+(1, 'Kawa'),
+(2, 'Napoje'),
+(3, 'Bułki'),
+(4, 'Na ciepło');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produkt`
 --
 
 CREATE TABLE `produkt` (
   `id` int(10) UNSIGNED NOT NULL,
   `nazwa` varchar(100) NOT NULL,
+  `kategoria_id` int(10) UNSIGNED NOT NULL,
   `cena` float NOT NULL,
   `dostepnosc` tinyint(1) NOT NULL,
   `promocja` float NOT NULL,
   `zdjecie` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Dumping data for table `produkt`
+--
+
+INSERT INTO `produkt` (`id`, `nazwa`, `kategoria_id`, `cena`, `dostepnosc`, `promocja`, `zdjecie`) VALUES
+(1, 'Espresso', 1, 1.5, 1, 0, '/resources/produkty/kawa/espresso.jpg'),
+(2, 'Espresso Macchiato', 1, 2.5, 1, 0, '/resources/produkty/kawa/espresso_macchiato.jpg'),
+(3, 'Kawa czarna', 1, 2, 1, 0, '/resources/produkty/kawa/czarna.jpg'),
+(4, 'Kawa biała', 1, 2.5, 1, 0, '/resources/produkty/kawa/biala.jpg'),
+(5, 'Cappuccino', 1, 3.5, 1, 0, '/resources/produkty/kawa/cappuccino.jpg'),
+(6, 'Latte macchiato', 1, 3.5, 1, 0, '/resources/produkty/kawa/latte_macchiato.jpg'),
+(7, 'Opłata za kubek', 1, 0.5, 1, 0, '/resources/produkty/kawa/oplata_za_kubek.jpg'),
+(8, 'Double shot espresso', 1, 3.5, 1, 0, '/resources/produkty/kawa/double_shot_espresso.jpg'),
+(9, 'Tymbark karton 1L', 2, 4.5, 1, 0, '/resources/produkty/napoje/tymbark_karton_1l.jpg'),
+(10, 'Woda gazowana', 2, 2.5, 1, 0, '/resources/produkty/napoje/woda_gazowana.jpg'),
+(12, 'Tymbark 2L', 2, 5, 1, 0, '/resources/produkty/napoje/tymbark_2l.jpg'),
+(13, 'Tymbark szkło 0.25L', 2, 2.5, 1, 0, '/resources/produkty/napoje/tymbark_szklo_025l.jpg'),
+(14, 'Tymbark plastik 0.5L', 2, 3, 1, 0, '/resources/produkty/napoje/tymbark_plastik_05l.jpg'),
+(15, 'Herbata', 2, 2.5, 1, 0, '/resources/produkty/napoje/herbata.jpg'),
+(16, 'B. Gołosza', 3, 4, 1, 0, '/resources/produkty/bulki/bulka_golosza.jpg'),
+(17, 'B. Ser', 3, 3, 1, 0, '/resources/produkty/bulki/bulka_ser.jpg'),
+(18, 'B. Szynka', 3, 3, 1, 0, '/resources/produkty/bulki/bulka_szynka.jpg'),
+(19, 'B. Szynka Ser', 3, 4, 1, 0, '/resources/produkty/bulki/bulka_szynka_ser.jpg'),
+(20, 'B. Sos', 3, 1, 1, 0, '/resources/produkty/bulki/bulka_sos.jpg'),
+(21, 'B. Masło', 3, 2, 1, 0, '/resources/produkty/bulki/bulka_maslo.jpg'),
+(22, 'B. Sucha', 3, 1.5, 1, 0, '/resources/produkty/bulki/bulka_sucha.jpg'),
+(23, 'B. Ciemna', 3, 4, 1, 0, '/resources/produkty/bulki/bulka_ciemna.jpg'),
+(24, 'Hot-dog', 4, 6, 1, 0, '/resources/produkty/na_cieplo/hot_dog.jpg'),
+(25, 'Double-dog', 4, 8, 1, 0, '/resources/produkty/na_cieplo/double_dog.jpg'),
+(26, 'Tost ser', 4, 2.5, 1, 0, '/resources/produkty/na_cieplo/tost_ser.jpg'),
+(27, 'Tost szynka', 4, 2.5, 1, 0, '/resources/produkty/na_cieplo/tost_szynka.jpg'),
+(28, 'Tost masło', 4, 1.5, 1, 0, '/resources/produkty/na_cieplo/tost_maslo.jpg'),
+(29, 'Tost ser szynka', 4, 4, 1, 0, '/resources/produkty/na_cieplo/tost_ser_szynka.jpg');
+
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `status`
+-- Table structure for table `status`
 --
 
 CREATE TABLE `status` (
@@ -60,7 +116,7 @@ INSERT INTO `status` (`id`, `nazwa`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `uprawnienia`
+-- Table structure for table `uprawnienia`
 --
 
 CREATE TABLE `uprawnienia` (
@@ -79,7 +135,7 @@ INSERT INTO `uprawnienia` (`id`, `nazwa`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `uzytkownik`
+-- Table structure for table `uzytkownik`
 --
 
 CREATE TABLE `uzytkownik` (
@@ -102,7 +158,7 @@ INSERT INTO `uzytkownik` (`id`, `login`, `email`, `haslo`, `imie`, `nazwisko`, `
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `zamowienie`
+-- Table structure for table `zamowienie`
 --
 
 CREATE TABLE `zamowienie` (
@@ -115,7 +171,7 @@ CREATE TABLE `zamowienie` (
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `zawartosc_zamowienia`
+-- Table structure for table `zawartosc_zamowienia`
 --
 
 CREATE TABLE `zawartosc_zamowienia` (
@@ -126,29 +182,36 @@ CREATE TABLE `zawartosc_zamowienia` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Indeksy dla zrzutów tabel
+-- Indexes for dumped tables
 --
 
 --
--- Indeksy dla tabeli `produkt`
+-- Indexes for table `kategoria`
 --
-ALTER TABLE `produkt`
+ALTER TABLE `kategoria`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeksy dla tabeli `status`
+-- Indexes for table `produkt`
+--
+ALTER TABLE `produkt`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `kategoria_id` (`kategoria_id`);
+
+--
+-- Indexes for table `status`
 --
 ALTER TABLE `status`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeksy dla tabeli `uprawnienia`
+-- Indexes for table `uprawnienia`
 --
 ALTER TABLE `uprawnienia`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeksy dla tabeli `uzytkownik`
+-- Indexes for table `uzytkownik`
 --
 ALTER TABLE `uzytkownik`
   ADD PRIMARY KEY (`id`),
@@ -157,7 +220,7 @@ ALTER TABLE `uzytkownik`
   ADD KEY `uprawnienia_id` (`uprawnienia_id`);
 
 --
--- Indeksy dla tabeli `zamowienie`
+-- Indexes for table `zamowienie`
 --
 ALTER TABLE `zamowienie`
   ADD PRIMARY KEY (`id`),
@@ -165,7 +228,7 @@ ALTER TABLE `zamowienie`
   ADD KEY `status_id` (`status_id`);
 
 --
--- Indeksy dla tabeli `zawartosc_zamowienia`
+-- Indexes for table `zawartosc_zamowienia`
 --
 ALTER TABLE `zawartosc_zamowienia`
   ADD PRIMARY KEY (`id`),
@@ -177,10 +240,16 @@ ALTER TABLE `zawartosc_zamowienia`
 --
 
 --
+-- AUTO_INCREMENT for table `kategoria`
+--
+ALTER TABLE `kategoria`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `produkt`
 --
 ALTER TABLE `produkt`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `status`
@@ -215,6 +284,12 @@ ALTER TABLE `zawartosc_zamowienia`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `produkt`
+--
+ALTER TABLE `produkt`
+  ADD CONSTRAINT `produkt_ibfk_1` FOREIGN KEY (`kategoria_id`) REFERENCES `kategoria` (`id`);
 
 --
 -- Constraints for table `uzytkownik`
