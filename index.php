10f6b2d4b92b376055a93c7e57596594ac693958
gelbel83 <pewnegorazukinga@gmail.com>
2026-05-27 23:26:10 +0200

    head w components

--- index.php
+++ index.php
@@ -7,21 +7,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>ZEGOWSKA SZAMA</title>
-
-        <meta lang="pl" />
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css" />
-
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Podkova:wght@400..800&display=swap" rel="stylesheet" />
-
-        <link rel="stylesheet" href="./style.css" type="text/css" />
-        <link rel="shortcut icon" href="resources/favicon.ico" type="image/x-icon" />
+        <?php create_head_tags(true)?>
 
         <script>
             const isLoggedIn = <?php echo isset($_SESSION["user"]) ? "true" : "false"; ?>;
@@ -32,7 +18,7 @@
     </head>
     
     <body class="d-flex flex-column">
-        <?php create_header(true) ?>
+        <?php create_header(true)?>
 
         <?php if (isset($_SESSION['user'])): ?>
         <section class="sales-section d-flex flex-column p-3 ">
