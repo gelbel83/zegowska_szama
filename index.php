60ba359409d0807dfe6b3a0ccb2fb67bb066f0f5
KianaEnjoyer <przewodnik180@gmail.com>
2026-05-28 18:20:01 +0200

    Naprawa produktów w bazie i wyswietlanie produktow w promocji

--- index.php
+++ index.php
@@ -24,16 +24,8 @@
         <section class="sales-section d-flex flex-column p-3 ">
             <h3>Po taniości</h3>
             <div class="sales-container h-25">
-                
-            </div>
-        </section>
-        <?php endif; ?>
-
-        <section class="d-flex flex-column p-3">
-            <h3>Co u nas wszamasz?</h3>
-            <div class="products-container">
-                 <?php 
-                    $products_sql = "SELECT *, produkt.nazwa AS nazwa_pr, kategoria.nazwa AS nazwa_kat FROM produkt JOIN kategoria on kategoria_id=kategoria.id;";
+                <?php 
+                    $products_sql = "SELECT *, produkt.nazwa AS nazwa_pr, kategoria.nazwa AS nazwa_kat FROM produkt JOIN kategoria on kategoria_id=kategoria.id WHERE produkt.promocja > 0;";
                     $products_arr = mysqli_select_no_parameters($products_sql);
                     if(!empty($products_arr)){
                         foreach($products_arr as $product){
@@ -45,9 +37,7 @@
                             echo number_format($product['cena'], 2, '.', '') . "zł";
                             echo "<br>";
                             if($product['promocja'] > 0){
-                                
                                 echo "-".$product['promocja'] . "%";
-                                
                             }
                             
                             if($product['dostepnosc'] == 1){
@@ -65,8 +55,44 @@
                             //ogarnac popupa dla usuwania
                         }
                     }
-                    else{
-                        echo "<h3> Brak danych </h3>";
+                ?>
+            </div>
+        </section>
+        <?php endif; ?>
+
+        <section class="d-flex flex-column p-3">
+            <h3>Co u nas wszamasz?</h3>
+            <div class="products-container">
+                <?php 
+                    $products_sql = "SELECT *, produkt.nazwa AS nazwa_pr, kategoria.nazwa AS nazwa_kat FROM produkt JOIN kategoria on kategoria_id=kategoria.id;";
+                    $products_arr = mysqli_select_no_parameters($products_sql);
+                    if(!empty($products_arr)){
+                        foreach($products_arr as $product){
+                            echo "<div class='product card m-1'>";
+
+                            echo "<h4>".$product['nazwa_pr']."</h4>"; 
+
+                            echo "Cena: ";
+                            echo number_format($product['cena'], 2, '.', '') . "zł";
+                            echo "<br>";
+                            if($product['promocja'] > 0){
+                                echo "-".$product['promocja'] . "%";
+                            }
+                            
+                            if($product['dostepnosc'] == 1){
+                                echo "dostępny";
+                            }
+                            else{
+                                echo "niedostępny";
+                            }
+                            echo "<br>";
+
+                            echo "<button>Dodaj do koszyka</button>";
+                            
+                            echo "</div>";
+                            //ogarnac popupa dla produktu wedlug id
+                            //ogarnac popupa dla usuwania
+                        }
                     }
                 ?>
             </div>
