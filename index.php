a19fc91f3b8d14197de3b96298faaaafded0f926
KianaEnjoyer <przewodnik180@gmail.com>
2026-05-28 18:03:50 +0200

    wyswietlanie produktow w stronie glowniej i dodanie podstrony zamowien

--- index.php
+++ index.php
@@ -32,7 +32,43 @@
         <section class="d-flex flex-column p-3">
             <h3>Co u nas wszamasz?</h3>
             <div class="products-container">
+                 <?php 
+                    $products_sql = "SELECT *, produkt.nazwa AS nazwa_pr, kategoria.nazwa AS nazwa_kat FROM produkt JOIN kategoria on kategoria_id=kategoria.id;";
+                    $products_arr = mysqli_select_no_parameters($products_sql);
+                    if(!empty($products_arr)){
+                        foreach($products_arr as $product){
+                            echo "<div class='product card m-1'>";
 
+                            echo "<h4>".$product['nazwa_pr']."</h4>"; 
+
+                            echo "Cena: ";
+                            echo number_format($product['cena'], 2, '.', '') . "zł";
+                            echo "<br>";
+                            if($product['promocja'] > 0){
+                                
+                                echo "-".$product['promocja'] . "%";
+                                
+                            }
+                            
+                            if($product['dostepnosc'] == 1){
+                                echo "dostępny";
+                            }
+                            else{
+                                echo "niedostępny";
+                            }
+                            echo "<br>";
+
+                            echo "<button>Dodaj do koszyka</button>";
+                            
+                            echo "</div>";
+                            //ogarnac popupa dla produktu wedlug id
+                            //ogarnac popupa dla usuwania
+                        }
+                    }
+                    else{
+                        echo "<h3> Brak danych </h3>";
+                    }
+                ?>
             </div>
         </section>
 
