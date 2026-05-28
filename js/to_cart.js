da9dd252e6ec56c818fa23205d863aa84c225dfa
KianaEnjoyer <przewodnik180@gmail.com>
2026-05-28 19:24:19 +0200

    obsługa dodawania do koszyka (sesji js)

--- js/to_cart.js
+++ js/to_cart.js
@@ -1,11 +1,21 @@
 "use strict";
 
 const allProductsButtons = document.querySelectorAll('.to-cart-button');
-sessionStorage.setItem('koszyk', '');
+sessionStorage.setItem('cart', '');
+
 allProductsButtons.forEach(product => {
     let product_id = product.id.slice(8);
     product.addEventListener("click", (e)=>{
         e.preventDefault();
-
+        let current_cart = [];
+        try{
+            current_cart = JSON.parse(sessionStorage.getItem('cart'));
+        }
+        catch{
+            
+        }
+        current_cart.push(product_id);
+        sessionStorage.setItem('cart', JSON.stringify(current_cart));
+        console.log(sessionStorage.getItem('cart'));
     })
 });
