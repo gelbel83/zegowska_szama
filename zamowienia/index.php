b2151e6d5b93d0ee54856ab0cc9ac9fd9dd4476d
gelbel83 <pewnegorazukinga@gmail.com>
2026-05-30 18:20:49 +0200

    jednak chce inna ikonke

--- zamowienia/index.php
+++ zamowienia/index.php
@@ -115,7 +115,7 @@
 
             <?php else: ?>
                 <div class="d-flex flex-column justify-content-center align-items-center flex-grow-1 text-center py-5">
-                    <i class="bi bi-box-seam text-muted" style="font-size: 5rem;"></i>
+                    <i class="bi bi-receipt-cutoff text-muted" style="font-size: 5rem;"></i>
                     <h3 class="mt-3">Nie masz jeszcze żadnych zamówień.</h3>
                 </div>
             <?php endif; ?>
