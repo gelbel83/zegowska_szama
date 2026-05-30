e216e373527f7b5838e7317114456b0ad0cfb5ec
gelbel83 <pewnegorazukinga@gmail.com>
2026-05-30 18:26:43 +0200

    czcionka

--- zamowienia/index.php
+++ zamowienia/index.php
@@ -115,8 +115,8 @@
 
             <?php else: ?>
                 <div class="d-flex flex-column justify-content-center align-items-center flex-grow-1 text-center py-5">
-                    <i class="bi bi-receipt-cutoff text-muted" style="font-size: 5rem;"></i>
-                    <h3 class="mt-3">Nie masz jeszcze żadnych zamówień.</h3>
+                    <i class="bi bi-receipt-cutoff" style="font-size: 5rem;"></i>
+                    <h3 class="fw-bold mt-3">Nie masz jeszcze żadnych zamówień</h3>
                 </div>
             <?php endif; ?>
 
